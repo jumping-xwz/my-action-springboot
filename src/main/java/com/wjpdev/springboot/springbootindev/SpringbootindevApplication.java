3b49f4d1b378f4a04660f65b0f0e11a4bf4b75e5
wjpdeveloper <wjpdev@gmail.com>
2017-10-28 15:13:52 +0800

    Add Spring-boot-start-hello spring-boot-autoconfigure

--- src/main/java/com/wjpdev/springboot/springbootindev/SpringbootindevApplication.java
+++ src/main/java/com/wjpdev/springboot/springbootindev/SpringbootindevApplication.java
@@ -1,8 +1,8 @@
 package com.wjpdev.springboot.springbootindev;
 
+import com.wjpdev.springboot.HelloService;
 import com.wjpdev.springboot.springbootindev.config.BookSettings;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,9 +15,17 @@ public class SpringbootindevApplication {
 	@Autowired
 	private BookSettings bookSettings;
 
+	@Autowired
+	HelloService helloService;
+
 	@RequestMapping("/")
 	String index(){
 		return "book name is: " + bookSettings.getName() + ", and book author is: " + bookSettings.getAuthor();
+	}
+
+	@RequestMapping("/hello")
+	public String hello(){
+		return helloService.sayHello();
 	}
 
 	public static void main(String[] args) {
